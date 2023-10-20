99b5a44d9789197c1a56b3d6ef004d845d8ed92c
Madai Arteaga <madai.arteaga@outlook.com>
2023-10-20 00:18:47 -0500

    R17 Add an example and install the needed packages
    This contains the base configuration and how to run.
    This will be configured after under nestJS so all of this
    will be executed under a service on the backend.

---
+++ src/utils/enums/scraping-pages.enum.ts
@@ -0,0 +1,12 @@
+export enum ScrapingPages {
+  SeminuevosHyundai = "https://seminuevos.hyundai.com.ec/busqueda",
+  SeminuevosKia = "https://seminuevos.com.ec/busqueda#resultados",
+  Autocor = "https://www.autocor.com.ec/vehiculos?page=1",
+  FullAutos = "https://www.full-autos.com/vehiculos",
+  Patiotuerca = "https://ecuador.patiotuerca.com/usados/-/autos?type_autos_moderated=moderated",
+  Nautos = "https://www.nautos.com.ec/stock-autos-semi-nuevos/",
+  Autocosmos = "https://www.autocosmos.com.ec/auto/usado",
+  Carros1001 = "https://www.1001carros.com/seminuevos",
+  Ambacar = "https://seminuevos.ambacar.ec/seminuevos",
+  MercadoLibre = "https://vehiculos.mercadolibre.com.ec/",
+}
---
+++ src/utils/scraping/seminuevos-hyundai.scraping.ts
@@ -0,0 +1,67 @@
+import puppeteer from "puppeteer";
+// import { ScrapingPages } from "@enums/scraping-pages.enum";
+
+export enum ScrapingPages {
+  SeminuevosHyundai = "https://seminuevos.hyundai.com.ec/busqueda",
+  SeminuevosKia = "https://seminuevos.com.ec/busqueda#resultados",
+  Autocor = "https://www.autocor.com.ec/vehiculos?page=1",
+  FullAutos = "https://www.full-autos.com/vehiculos",
+  Patiotuerca = "https://ecuador.patiotuerca.com/usados/-/autos?type_autos_moderated=moderated",
+  Nautos = "https://www.nautos.com.ec/stock-autos-semi-nuevos/",
+  Autocosmos = "https://www.autocosmos.com.ec/auto/usado",
+  Carros1001 = "https://www.1001carros.com/seminuevos",
+  Ambacar = "https://seminuevos.ambacar.ec/seminuevos",
+  MercadoLibre = "https://vehiculos.mercadolibre.com.ec/",
+}
+
+async function scrapingHyundai() {
+  // Launch the browser and open a page
+  const browser = await puppeteer.launch({
+    // headless: false, //TODO: This should be true or delete
+    // devtools: true, //TODO: This delete
+    executablePath:
+      "/nix/store/0pj6lchxhdnv7kxbz8g35028j4bx07gi-user-environment/bin/chromium",
+  });
+  const page = await browser.newPage();
+
+  // Navigate to the page
+  await page.goto(ScrapingPages.SeminuevosHyundai);
+
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  await page.waitForSelector(".card-title strong");
+
+  // Find the element with the id "resultados."
+  const resultadosElement = await page.$("#resultados");
+
+  if (resultadosElement) {
+    // Use page.$$eval to iterate through elements with the class "card-title strong" and extract the text.
+    const cardTitleText = await resultadosElement.$$eval(
+      ".card-title strong",
+      (elements) => {
+        return elements.map((element) => element.textContent?.trim());
+      },
+    );
+
+    // Use page.$$eval to find elements with the class "record" and extract the number.
+    const recordNumber = await resultadosElement.$$eval(
+      ".record",
+      (elements) => {
+        return elements.map((element) => element.textContent?.trim());
+      },
+    );
+
+    // Now, 'cardTitleText' contains an array of text, and 'recordNumber' contains an array of numbers.
+    console.log("Card Title Text:", cardTitleText);
+    console.log("Record Number:", recordNumber);
+  }
+
+  //   await browser.close();
+}
+
+scrapingHyundai();
+
+/*
+yarn ts-node --esm src/utils/scraping/seminuevos-hyundai.scraping.ts
+*/
